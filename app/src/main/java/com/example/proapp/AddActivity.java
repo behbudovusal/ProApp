6172e2a394287ce57c7b103c3b9d2222a60b27e8
Vusal_ <behbudovb@gmail.com>
2019-10-16 03:04:15 +0400

    Second commit

--- app/src/main/java/com/example/proapp/AddActivity.java
+++ app/src/main/java/com/example/proapp/AddActivity.java
@@ -1,14 +1,48 @@
 package com.example.proapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
 
+    EditText datetext;
+    DatePickerDialog picker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        datetext=(EditText) findViewById(R.id.date);
+        datetext.setInputType(InputType.TYPE_NULL);
+        datetext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(AddActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                datetext.setText(dayOfMonth + "." + (monthOfYear + 1) + "." + year);
+                            }
+                        }, year, month, day);
+
+                picker.setButton(DatePickerDialog.BUTTON_NEGATIVE,"Legv et",picker);
+                picker.show();
+
+
+            }
+        });
     }
 }
